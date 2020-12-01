d5878afc2afbf4f86a82e6f87bd215c9a417574a
Jhon Garcia <metalprime57@gmail.com>
2020-12-01 11:39:47 -0500

    connection firebase

--- app/src/main/java/com/example/parcial2androidecoapp/MainActivity.java
+++ app/src/main/java/com/example/parcial2androidecoapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.parcial2androidecoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView currentQuestion;
+    private EditText inputScore;
+    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOk = findViewById(R.id.btnOk);
+        inputScore = findViewById(R.id.inputScore);
+        currentQuestion = findViewById(R.id.currentQuestion);
+
+
     }
 }
